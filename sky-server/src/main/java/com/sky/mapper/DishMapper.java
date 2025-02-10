4c95bc01c3c308665162aaab12f33c5fa72f95cc
Bkornblume <kinoko84@outlook.com>
2025-02-10 21:38:44 +0900

    公共字段自动填充 共通フィールドを自動補完

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -21,4 +21,13 @@ public interface DishMapper {
     Dish getById(Long id);
 
     Page<Dish> pageQuery(DishPageQueryDTO dishPageQueryDTO);
+
+
+    /**
+     * 根据分类id查询菜品数量
+     * @param categoryId
+     * @return
+     */
+    @Select("select count(id) from dish where category_id = #{categoryId}")
+    Integer countByCategoryId(Long categoryId);
 }
